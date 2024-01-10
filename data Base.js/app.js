8bd85872958ecc8ba1fcf9bd87036f431516b0e9
Mateo020528 <149026537+Mateo020528@users.noreply.github.com>
2024-01-10 18:51:36 -0500

    Update app.js

--- data Base.js/app.js
+++ data Base.js/app.js
@@ -1,18 +1,46 @@
 //se obtienen los ID`S
-let resultado = fetch('https://memin.io/public/api/users')
-//console.log(resultado) //n usamos este console.log para saber que nos devuelce la promesa
-.then(response=> {
-    return response.json()
-}).then(data => {
+let resultado = fetch("https://memin.io/public/api/users")
+  //console.log(resultado) //n usamos este console.log para saber que nos devuelce la promesa
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
     //creamos lo elementos
-    data.forEach(function(element){
-       // console.log(element.name)//n Este console.log es para mostrar cada nombre de nuestro usuario en consola.
-    })
-})
-//agregar mas td a tbody
-let td = document.createElement('td');
-tr.appendChild(td);
+    data.forEach(function (element) {
+      // console.log(element.name)//n Este console.log es para mostrar cada nombre de nuestro usuario en consola.
+      //crear un tr y agregarlo al los td
+    let tr = document.createElement("tr");
+    let td = document.createElement("td");  
+    td.innerHTML= element.id;
+    tr.appendChild(td);
+
+    let name = document.createElement('td');
+    name.innerHTML= element.name;
+    tr.appendChild(name);
+    
+    let email = document.createElement('td');
+    email.innerHTML= element.email;
+    tr.appendChild(email);
 
 
+    let acciones = document.createElement('td');
 
+    let editar = document.createElement('a');
+    editar.setAttribute('href','#');
+    editar.textContent ='Editar'
+    acciones.appendChild(editar);
+    tr.appendChild(acciones);
+
+    // let detalles = document-createElement('a');
+    // detalles.setAttribute('href','#');
+    // detalles.textContent='Detalles'
+    // acciones.appendChild(detalles);
+    // tr.appendChild(acciones);
+    
+
+
+    tbody = document.getElementById("tbody");
+    tbody.appendChild(tr);
+    }); 
+});
 
